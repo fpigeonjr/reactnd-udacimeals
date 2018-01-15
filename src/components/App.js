69fa81617ca2bc99305383b1d82062d357475a20
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2018-01-14 23:23:35 -0600

    adds react-redux. uses connect to pass store to app component

--- src/components/App.js
+++ src/components/App.js
@@ -1,49 +1,35 @@
 import React, { Component } from "react";
-import { addRecipe } from "../actions";
+import { connect } from "react-redux";
 
 class App extends Component {
-  state = {
-    calendar: null
-  };
-
-  componentDidMount() {
-    const { store } = this.props;
-    store.subscribe(() => {
-      this.setState(() => ({
-        calendar: store.getState()
-      }));
-    });
-  }
-
-  submitFood = () => {
-    this.props.store.dispatch(
-      addRecipe({
-        day: "Monday",
-        meal: "breakfast",
-        recipe: { label: this.input.value }
-      })
-    );
-
-    this.input.value = "";
-  };
-
   render() {
-    return (
-      <div>
-        <input
-          type="text"
-          ref={input => (this.input = input)}
-          placeholder="Monday's Breakfast"
-        />
-        <button onClick={this.submitFood}>Submit</button>
-
-        <pre>
-          Monday's Breakfast:{" "}
-          {this.state.calendar && this.state.calendar.monday.breakfast}
-        </pre>
-      </div>
-    );
+    console.log("props is", this.props);
+    return <div>Hello World</div>;
   }
 }
 
-export default App;
+function mapStateToProps(calendar) {
+  const dayOrder = [
+    "sunday",
+    "monday",
+    "tuesday",
+    "wednesday",
+    "thursday",
+    "friday",
+    "saturday"
+  ];
+  return {
+    calendar: dayOrder.map(day => ({
+      day,
+      meals: Object.keys(calendar[day]).reduce((meals, meal) => {
+        meals[meal] = calendar[day][meal]
+        ? calendar[day][meal]
+        : null
+
+      return meals
+      }, {})
+    }))
+  };
+}
+
+export default connect(mapStateToProps)(App);
