0483dd3e2a2c090bb1981219c46f7a9ac9628a56
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2018-01-16 15:38:35 -0600

    adds combineReducers from Redux

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,19 +1,18 @@
+import {combineReducers} from "redux";
 import { ADD_RECIPE, REMOVE_FROM_CALENDAR } from "../actions";
 
-function food(state = {}, action){
-  switch(action.type){
+function food(state = {}, action) {
+  switch (action.type) {
     case ADD_RECIPE:
-     const {recipe} = action
+      const { recipe } = action;
 
-     return {
-       ...state,
-       [recipe.label]: recipe
-    }
+      return {
+        ...state,
+        [recipe.label]: recipe
+      };
     default:
-      return state
+      return state;
   }
-
-
 }
 
 const initialCalendarState = {
@@ -79,4 +78,4 @@ function calendar(state = initialCalendarState, action) {
   }
 }
 
-export default calendar;
+export default combineReducers({ calendar, food });
